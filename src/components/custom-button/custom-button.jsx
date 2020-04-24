b1873da311185834119691d07d8d4d8e2400701d
Shane O'Mahoney <Shane.O'Mahoney@pdms.com>
2020-04-24 16:17:13 +0100

    Updated shop data with new data.  Updated card utils to group items in card reducer

--- src/components/custom-button/custom-button.jsx
+++ src/components/custom-button/custom-button.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 
 import './custom-button.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-    <button
-        className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
-        {...otherProps}
-    >
-        {children}
-    </button>
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+  <button className={`${isGoogleSignIn ? 'google-sign-in' : ''}${inverted ? 'inverted' : ''} custom-button`} {...otherProps}>
+    {children}
+  </button>
 );
 
 export default CustomButton;
