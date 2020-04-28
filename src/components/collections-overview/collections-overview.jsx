36a781ee056483c8a693ca73a5b0aa696ce7b8f1
Shane O'Mahoney <Shane.O'Mahoney@pdms.com>
2020-04-28 13:49:24 +0100

    Created selectors and reducers for rest of components

--- src/components/collections-overview/collections-overview.jsx
+++ src/components/collections-overview/collections-overview.jsx
@@ -3,20 +3,20 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview';
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import './collections-overview.scss';
 
 const CollectionOverview = ({ collections }) => (
-    <div className="collections-overview">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-    </div>
+  <div className="collections-overview">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionOverview from '../../components/collections-overview/collections-overview';
+import CollectionPage from '../collection/collection';
 
-const ShopPage = () => (
-    <div className="shop-page">
-        <CollectionOverview />
-    </div>
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
 );
 
 export default ShopPage;
